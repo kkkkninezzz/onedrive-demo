7fe5ec171617fac04828f9fded6883575750ad79
18581489108 <18581489108@163.com>
2021-08-17 18:33:26 +0800

    初始化项目

---
+++ src/main/java/cn/kurisu9/onedrivedemo/OnedriveDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.kurisu9.onedrivedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnedriveDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnedriveDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/kurisu9/onedrivedemo/OnedriveDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.kurisu9.onedrivedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnedriveDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
